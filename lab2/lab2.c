27926057472b1ce712d958839ec0b159b9ee4602
joshuaG <joshua.guzman.7@my.csun.edu>
2025-10-26 02:09:07 -0700

    finished lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <limits.h>
 
-typedef struct Table
-{
+typedef struct Table{
     int id;
     int arrival;
     int total;
     int start;
     int end;
     int turnAround;
+    int done;
+    int already_start;
+    int totalRemaining;
 }TableInfo;
 
 TableInfo *tbPtr;
@@ -19,8 +21,10 @@ void printTableInfo();
 void intializeTable();
 void firstComeFirstServed();
 void shortestJobFirst();
-void shortReaminingTime();
+void shortRemainingTime();
 void terminateProg();
+
+
 
 int main () {
     int choice = 0;
@@ -44,6 +48,15 @@ int main () {
         case 1:
             intializeTable();
             printTableInfo();
+            break;
+        case 2:
+            firstComeFirstServed();
+            break;
+        case 3:
+            shortestJobFirst();
+            break;
+        case 4:
+            shortRemainingTime();
             break;
         case 5:
             terminateProg();
@@ -103,10 +116,119 @@ void printTableInfo(){
 
 }
 
+void firstComeFirstServed() {
+   // for each process, reset "done" field to 0
+   for (int i = 0; i < tableSize; i++){
+    tbPtr[i].done = 0;
+   }
+   int sum = 0;
+   int i = 0;
+   while (i < tableSize){
+    // keep track of what process is has the earliest arrival time 
+    int indexArr;
+    int earliestArrival = INT_MAX;
+    
+    
+    for (int j = 0; j < tableSize; j++){
+        //check if curr process j has been schedule or not so done = 0
+        if (tbPtr[j].done == 0){
+            //check if curr arrival time for j is smaller than value in earliestArrival if so set new min
+            if (tbPtr[j].arrival < earliestArrival){
+                earliestArrival = tbPtr[j].arrival;
+                indexArr = j;
+            }
 
-void firstComeFirstServed(void) {}
-void shortestJobFirst(void) {}
-void shortRemainingTime(void) {}
+        }
+    }
+
+    //calculate start, end, turnaround for earliest process
+    tbPtr[indexArr].start = sum;
+    tbPtr[indexArr].end = sum + tbPtr[indexArr].total;
+    tbPtr[indexArr].turnAround = tbPtr[indexArr].end - tbPtr[indexArr].arrival;
+    tbPtr[indexArr].done = 1; 
+
+    sum = tbPtr[indexArr].end;
+    i++;
+   }
+   printTableInfo();
+}
+
+void shortestJobFirst() {
+    for (int i = 0; i < tableSize; i ++){
+        tbPtr[i].done = 0;
+    }
+
+    int i = 0;
+    int sum = 0;
+    while (i < tableSize) {
+        int indexTotal;
+        int smallestBurstTime = INT_MAX;
+
+        for (int j = 0; j < tableSize; j++){
+            if (tbPtr[j].done == 0 && tbPtr[j].arrival <= sum){
+                if (tbPtr[j].total < smallestBurstTime ){
+                    smallestBurstTime = tbPtr[j].total;
+                    indexTotal = j;
+                }
+            }
+        }
+
+        tbPtr[indexTotal].start = sum;
+        tbPtr[indexTotal].end = sum + tbPtr[indexTotal].total;
+        tbPtr[indexTotal].turnAround = tbPtr[indexTotal].end - tbPtr[indexTotal].arrival;
+        tbPtr[indexTotal].done = 1;
+
+        sum = tbPtr[indexTotal].end;
+        i++;
+    }
+
+    printTableInfo();
+}
+
+void shortRemainingTime() {
+    for (int i = 0; i < tableSize; i++){
+        tbPtr[i].done = 0;
+        tbPtr[i].already_start = 0;
+        tbPtr[i].totalRemaining = tbPtr[i].total;
+    }
+
+
+    int current_time = 0;
+    int i = 0;
+    while (i < tableSize) {
+        int index = -1;
+        int lowRemainingT = INT_MAX;
+
+        for(int j = 0; j < tableSize; j++){
+            if (tbPtr[j].done == 0 && tbPtr[j].arrival <= current_time){
+                if (tbPtr[j].totalRemaining < lowRemainingT){
+                    lowRemainingT = tbPtr[j].totalRemaining;
+                    index = j;
+                }
+
+            }
+
+        }
+
+        // check if the process picked has been previously schedule if not then toogle to yes 
+        if (tbPtr[index].already_start == 0){
+            tbPtr[index].already_start = 1;
+            tbPtr[index].start = current_time;
+        } 
+        
+
+        tbPtr[index].totalRemaining -= 1;
+        current_time += 1;
+
+        if (tbPtr[index].totalRemaining == 0){
+            tbPtr[index].end = current_time;
+            tbPtr[index].turnAround = tbPtr[index].end - tbPtr[index].arrival;
+            tbPtr[index].done = 1;
+            i++;
+        }
+    }
+    printTableInfo();
+}
 
 void terminateProg(){
     free(tbPtr);
